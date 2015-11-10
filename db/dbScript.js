6004bcca3a081805e8ff0572ae45a5b353583d98
rainbow494 <rainbow494@gmail.com>
2015-11-10 23:46:27 +0800

    Updated UI Added update collection statements into db script

--- db/dbScript.js
+++ db/dbScript.js
@@ -6,3 +6,10 @@ db.lunch.insert({name:'weipu', account: 0});
 db.lunch.insert({name:'chen', account: 0});
 db.lunch.insert({name:'paul', account: 0});
 
+
+db.lunch.update({name:'yuki'},{$set: {account: 100}});
+db.lunch.update({name:'nick'},{$set: {account: 100}});
+db.lunch.update({name:'tony'},{$set: {account: 100}});
+db.lunch.update({name:'weipu'},{$set: {account: 100}});
+db.lunch.update({name:'chen'},{$set: {account: 100}});
+db.lunch.update({name:'paul'},{$set: {account: 100}});
