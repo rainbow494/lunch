a69f7f5e16467bd960117b66e42958422d8f582c
rainbow494 <rainbow494@gmail.com>
2015-11-15 22:40:36 +0800

    use requirejs to refactor code

--- webSite/project/js/main.js
+++ webSite/project/js/main.js
@@ -1,31 +1,39 @@
 ;
-(function () {
-	
+require.config({
+    baseUrl : 'bower_components',
+    paths : {
+        jquery : 'jquery/dist/jquery',
+        knockout : 'knockout/dist/knockout'
+    }
+});
+
+require(['jquery', 'knockout'], function ($, ko) {
+    
     var obLunchAccounts = ko.observableArray();
-	
-	myViewModel = {
-		obLunchAccounts
-	};
-	
+
+    myViewModel = {
+        obLunchAccounts
+    };
+
     var loadPage = function () {
         loadData();
-	}
-	
+    }
+
     var loadData = function () {
         var url = 'http://localhost:3000/api/lunch/summary';
         $.ajax({
             url : url,
             beforeSend : function (xhr) {
                 xhr.overrideMimeType("text/plain; charset=x-user-defined");
-			}
-		})
+            }
+        })
         .done(function (data) {
-			var result = $.parseJSON(data);
+            var result = $.parseJSON(data);
             obLunchAccounts(result);
-		});
-	}
-	
-	loadPage();
-    
+        });
+    }
+
+    loadPage();
+
     ko.applyBindings(myViewModel);
-})();
+});
