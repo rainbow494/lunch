8a5b52a1706a4973b207aa082af68b6a8f79ea53
rainbow494 <rainbow494@gamil.com>
2015-12-03 16:02:41 +0800

    Refine package.json

--- backend/scheduleService.js
+++ backend/scheduleService.js
@@ -1,15 +1,15 @@
-(function () {
+(function() {
     var CronJob = require('cron').CronJob,
-    mailHelper = require('./mailHelper.js').mailHelper();
+        mailHelper = require('./mailHelper.js').mailHelper();
 
     var weeklyReportScheduleJob = new CronJob({
-            cronTime : '00 30 13 * * 5',
-            onTick : function () {
-                mailHelper.sendWeeklyReports();
-            },
-            start : false,
-            timeZone : "Asia/Shanghai"
-        });
+        cronTime: '00 30 13 * * 5',
+        onTick: function() {
+            mailHelper.sendWeeklyReports();
+        },
+        start: false,
+        timeZone: "Asia/Shanghai"
+    });
     weeklyReportScheduleJob.start();
 
     console.log('Schedule services start');
