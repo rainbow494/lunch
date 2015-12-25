763bd7ccf2b95375bfdfb170fa77ec399a4c7f90
rainbow494 <rainbow494@gmail.com>
2015-12-26 00:45:22 +0800

    commit template

--- db/dbScript.js
+++ db/dbScript.js
@@ -1,3 +1,4 @@
+/* jshint ignore:start */
 // Create Master Collection
 db.lunch.ensureIndex({name:1}, {unique:true});
 db.lunch.insert({name:'yuki', account: 0});
@@ -21,6 +22,10 @@ db.lunch.update({name:'weipu'},{$set: {mail: '94178875@qq.com'}});
 db.lunch.update({name:'chen'},{$set: {mail: 'chenhao.gao@hotmail.com'}});
 db.lunch.update({name:'paul'},{$set: {mail: 'rainbow494@qq.com'}});
 
+//Todo : add index of table
+
+//db.yuki_detail.remove({});
+
 // Create Detail Collection
 db.counters.ensureIndex({_id:1}, {unique:true});
 db.counters.insert({_id: 'yuki_detail_seq', seq: 0});
@@ -29,13 +34,30 @@ function getNextSequence(name) {
    var ret = db.counters.findAndModify(
           {
             query: { _id: name },
-            update: { $inc: { seq: 0 } },
+            update: { $inc: { seq: 1 } },
             new: true
           }
    );
 
    return ret.seq;
-}
+};
+//tzoffset = (new Date()).getTimezoneOffset() * 60000;
+//db.yuki_detail.insert({_id: getNextSequence('yuki_detail_seq'), amount: '0', update:(new Date(Date.now() - tzoffset)).toISOString().slice(0,-1).split('T')[0] });
+db.detail.insert({_id: getNextSequence('yuki_detail_seq'), name:'yuki', amount: -10, date:new Date('2015-12-01')});
 
-// Todo : add index of table
-db.yuki_detail.insert({_id: getNextSequence('yuki_detail_seq'), amount: '0', update:new Date() });
+db.detail.aggregate([{$group:{_id: "$name",totalAmount: { $sum: "$amount" }}},{ $out : "tmp" }]);
+
+db.lunch.find().forEach(function (userAccount) {
+    var tmp = db.tmp.find({ _id: userAccount.name }, { totalAmount: 1 });
+        userAccount.account = tmp.totalAmount;
+        db.lunch.save(userAccount);
+});
+
+
+db.tmp.find().forEach(function(tmp){
+       db.lunch.findAndModify({
+            query:{name:tmp._id},
+            update:{account:tmp.totalAmount}
+       });
+});
+/* jshint ignore:end */
